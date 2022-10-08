e5ee79e351b76d4db12593b85442031b72ace134
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2022-10-08 21:39:48 +0300

    static google chart kullandık area da

---
+++ CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,31 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StaticCategoryChart()
+        {
+            List<CategoryClass> chartList = new List<CategoryClass>();
+
+            chartList.Add(new CategoryClass { CategoryCount = 78, CategoryName = "Teknoloji" });
+            chartList.Add(new CategoryClass { CategoryCount = 30, CategoryName = "Yazılım" });
+            chartList.Add(new CategoryClass { CategoryCount = 56, CategoryName = "Spor" });
+            chartList.Add(new CategoryClass { CategoryCount = 98, CategoryName = "Sağlık" });
+            chartList.Add(new CategoryClass { CategoryCount = 65, CategoryName = "Medya" });
+
+            return Json(new { jsonList=chartList});
+        }
+    }
+}
