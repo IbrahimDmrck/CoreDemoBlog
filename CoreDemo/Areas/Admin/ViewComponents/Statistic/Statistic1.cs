50b9fd81a2272fed88830c38346174f164f8e9c1
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2022-10-08 20:11:52 +0300

    Api üzerinden anlık hava durumu bilgisi admin sayfasına çekildi ve havanın durumuna göre hava durumu iconu değiştirilme işlemi gerçekleştirildi

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -19,6 +20,15 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.ContactCount = context.Contacts.Count();
             ViewBag.CommentCount = context.Comments.Count();
             ViewBag.LastBLog = context.Blogs.OrderByDescending(x => x.BlogID).Select(x => x.BlogTitle).Take(1).FirstOrDefault();
+
+            string api = "53c9a89b46fb2bcee8bbb8781789a6c4";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=Yalova&mode=xml&lang=tr&units=metric&appid="+api;
+            XDocument document = XDocument.Load(connection);
+
+            ViewBag.Weather = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+            ViewBag.WeatherStatus = document.Descendants("weather").ElementAt(0).Attribute("value").Value;
+
             return View();
         }
     }
