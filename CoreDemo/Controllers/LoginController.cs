8a33172c04b8dbfdf58a63ad8f401f9f692968ae
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2022-10-12 10:33:58 +0300

    identity üzerinden sisteme login olunması tamamlandı

--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -1,8 +1,10 @@
-﻿using DataAccess.Concrete;
+﻿using CoreDemo.Models;
+using DataAccess.Concrete;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -15,6 +17,12 @@ namespace CoreDemo.Controllers
     [AllowAnonymous]
     public class LoginController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
 
         public IActionResult Index()
         {
@@ -22,31 +30,49 @@ namespace CoreDemo.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Index(Writer writer)
+        public async Task<IActionResult> Index(UserSignInViewModel p)
         {
-            // burada yapılan işlem solide uygun değildir düzeltilecektir.
-            //burada yapılan işlem kullnıcının oturum açma işlemidir
-            BlogContext context = new BlogContext();
-            var dataValue = context.Writers.FirstOrDefault(x => x.WriterMail == writer.WriterMail && x.WriterPassword == writer.WriterPassword);
-            if (dataValue != null)
+            if (ModelState.IsValid)
             {
-                var claims = new List<Claim>
+                var result = await _signInManager.PasswordSignInAsync(p.UserName, p.Password, false, true);
+                if (result.Succeeded)
                 {
-                    new Claim(ClaimTypes.Name,writer.WriterMail)
-                };
-                var userIdentity = new ClaimsIdentity(claims, "a");
-                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
-                await HttpContext.SignInAsync(principal);
-
-                return RedirectToAction("Index", "Dashboard");
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Login");
+                }
             }
-            else
-            {
-                return View();
-            }
-
-
+            return View();
         }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Index(Writer writer)
+        //{
+        //    // burada yapılan işlem solide uygun değildir düzeltilecektir.
+        //    //burada yapılan işlem kullnıcının oturum açma işlemidir
+        //    BlogContext context = new BlogContext();
+        //    var dataValue = context.Writers.FirstOrDefault(x => x.WriterMail == writer.WriterMail && x.WriterPassword == writer.WriterPassword);
+        //    if (dataValue != null)
+        //    {
+        //        var claims = new List<Claim>
+        //        {
+        //            new Claim(ClaimTypes.Name,writer.WriterMail)
+        //        };
+        //        var userIdentity = new ClaimsIdentity(claims, "a");
+        //        ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+        //        await HttpContext.SignInAsync(principal);
+
+        //        return RedirectToAction("Index", "Dashboard");
+        //    }
+        //    else
+        //    {
+        //        return View();
+        //    }
+
+
+        //}
     }
 }
 
