9f864c1ebcd0d197e1adf46754fc5512ffcc2247
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2022-10-16 19:03:32 +0300

    yazar panelinde yeni blog oluşturmada min blogcontent sayısı 160 a ayarlandı ve eklem işlemi sırasında textarea da kaç karakter yazdığımızı göstern script yazıldı aynı işlem blog güncellemede de yapıldı , admin panelinde bloglar getirildi

--- BusinessLayer/ValidationRules/BlogValidator.cs
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -14,6 +14,7 @@ namespace BusinessLayer.ValidationRules
         {
             RuleFor(x => x.BlogTitle).NotEmpty().WithMessage("Blog başlığını boş geçemezsiniz");
             RuleFor(x => x.BlogContent).NotEmpty().WithMessage("Blog içeriğini boş geçemezsiniz");
+            RuleFor(x => x.BlogContent).MinimumLength(160).WithMessage("En az 160 karakter girmelisiniz");
             RuleFor(x => x.BlogImage).NotEmpty().WithMessage("Blog görseli eklemelisiniz");
             RuleFor(x => x.BlogTitle).MinimumLength(5).WithMessage("Bu alana en az 5 karakter girebilirsiniz");
             RuleFor(x => x.BlogTitle).MaximumLength(150).WithMessage("Bu alana en fazla 150 karakter girebilirsiniz");
---
+++ CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,80 @@
+﻿using BusinessLayer.Concrete;
+using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminMessageController : Controller
+    {
+        Message2Manager message2Manager = new Message2Manager(new EfMessage2Repository());
+       
+        BlogContext context = new BlogContext();
+
+        public IActionResult InBox()
+        {
+            var userName = User.Identity.Name;
+            var userMail = context.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var WriterId = context.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
+
+            var values = message2Manager.GetInboxListByWriter(WriterId);
+            return View(values);
+        }
+
+        public IActionResult SendBox()
+        {
+            var userName = User.Identity.Name;
+            var userMail = context.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var WriterId = context.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
+
+
+            var values = message2Manager.GetSendboxListByWriter(WriterId);
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult ComposeMessage()
+        {
+            GetReceiverSelectListItem();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ComposeMessage(Message2 message2)
+        {
+            var userName = User.Identity.Name;
+            var userMail = context.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var WriterId = context.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
+            
+            
+
+            message2.SenderID = WriterId;
+            message2.MessageStatus = true;
+            message2.MessageDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            message2Manager.TAdd(message2);
+            GetReceiverSelectListItem();
+            return RedirectToAction("SendBox", "AdminMessage");
+        }
+
+        public void GetReceiverSelectListItem()
+        {
+
+
+            UserManager cm = new UserManager(new EfUserRepository());
+            List<SelectListItem> recieverUsers = (from x in cm.GetReceiverUsersList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.Email.ToString(),
+                                                      Value = x.Id.ToString()
+                                                  }).ToList();
+            ViewBag.RecieverUser = recieverUsers;
+        }
+    }
+}
